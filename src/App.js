0878e4fa5c1a45f43239d550bb5142642e11f5f0
Diego Mego <dmego@chasquitechnologies.com>
2023-03-07 00:41:23 +0000

    feat: finished all features. missing typescript

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import "./App.css";
 import 'react-loading-skeleton/dist/skeleton.css';
 
-import "./fonts/helvetica/Helvetica_Normal.ttf";
-import "./fonts/helvetica/Helvetica_Bold.ttf";
 import PeopleContainer from "./PeopleContainer";
 import PersonContainer from "./PersonContainer";
 
